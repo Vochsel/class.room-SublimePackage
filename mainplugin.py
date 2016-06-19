4e4c2edf07e9aa0d4763340b0e54cefacbaa1af7
Ben Skinner <benjamin.skinner96@gmail.com>
2016-06-19 17:08:39 +1000

    Added filename and json encoding

--- mainplugin.py
+++ mainplugin.py
@@ -2,6 +2,9 @@ import sublime, sublime_plugin
 
 import socket
 import sys
+import json
+
+import os
 
 ip = "localhost"
 port = 5000
@@ -13,7 +16,7 @@ def Connect():
 	sock.connect((ip, port))
 
 Connect()
-
+ 
 class ClassRoomBroadcast(sublime_plugin.EventListener):
 	def on_modified_async(this, view):
 		curPos = view.sel()[0].a - 1
@@ -21,5 +24,21 @@ class ClassRoomBroadcast(sublime_plugin.EventListener):
 		curChar = view.substr(curPos)
 		curLine = view.substr(view.line(curReg));
 		contents = view.substr(sublime.Region(0, view.size()));
-		sock.send(contents.encode())
-		print("Sent packet")
+
+		#Current document syntax
+		language = os.path.splitext(os.path.basename(view.settings().get('syntax')))[0]
+		language = language.lower()
+		if language == "html":
+			language = "htmlmixed"
+
+		#Filename
+		if view.file_name():
+			filename = os.path.basename(view.file_name())
+		else:
+			filename = "untitled"
+		
+		#JSON packet to send
+		packet = json.dumps({'lang' : language, 'file' : {'name' : filename, 'contents' : contents}});
+
+		sock.send(packet.encode())
+		print("Sent packet from file: " + filename) 
